4132349afe0cd17a1a0c11b9f68c78c5e7780888
Daniel Lins <daniel.lins@arista.com.br>
2020-08-14 15:48:55 -0300

    Criei um metodo de negociação setando o objeto data.

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -9,8 +9,22 @@ class NegociacaoController {
     }
 
     adiciona(event) {
+
         event.preventDefault();
 
-        let data = new Date(this._inputData.value.replace(/-/g, ','));
+        let data = new Date(...
+            this._inputData.value
+            .split('-')
+            .map(function (item, indice) {
+                return item - indice % 2;
+            }));
+
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+
+        console.log(negociacao);
     }
 }
