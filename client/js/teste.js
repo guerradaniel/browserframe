71e63bfaabe065c5d34d3231ec9b0fdf8f14a3e3
Daniel Lins <daniel.lins@arista.com.br>
2020-08-17 18:30:41 -0300

    Realizei testes e criei classes auxiliares para setar e isolar datas.

--- client/js/teste.js
+++ client/js/teste.js
@@ -1,8 +1,61 @@
-const str = 'Daniel 0 Guerra';
-const vogais = 'aeiou'.split('');
-const parts = str
-    .split('')
-    .filter(letra => vogais.indexOf(letra) < 0)
-    .join('');
+let numeros = [3, 2, 11, 20, 8, 7];
 
-console.log(parts);
+let resultado = numeros.map((item) => {
+    if ((item % 2)) return item * 2;
+
+    return item;
+})
+console.log("Primeiro teste");
+console.log("primeiro resultado: ", resultado);
+
+/****************************/
+
+let array = [10, 50];
+
+function somaDoisNumeros(n1, n2) {
+    return n1 + n2;
+}
+console.log("Segundo teste")
+console.log(somaDoisNumeros(...array));
+
+/****************************/
+
+class Aluno {
+    constructor(matricula, nome) {
+        this.matricula = matricula;
+        this.nome = nome;
+    }
+}
+
+class Prova {
+    constructor(aluno, nota) {
+        this.aluno = aluno;
+        this.nota = nota;
+    }
+}
+
+
+let avaliacoes = [
+    new Prova(new Aluno(1, 'Luana'), 8),
+    new Prova(new Aluno(2, 'Cassio'), 6),
+    new Prova(new Aluno(3, 'Barney'), 9),
+    new Prova(new Aluno(4, 'Bira'), 5)
+];
+
+
+
+
+let aprovados = avaliacoes
+    .filter(function (prova) {
+        return prova.nota >= 7;
+    }).map(function (prova) {
+        return prova.aluno.nome;
+    });
+
+let reprovados = avaliacoes
+    .filter((prova) => prova.nota <= 7)
+    .map((prova) => prova.aluno.nome);
+
+
+console.log("Sem arrow functions(aprovados): ", aprovados);
+console.log("Com arrow functions(reprovados): ", reprovados);
